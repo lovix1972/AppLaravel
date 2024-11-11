f2ed7145e290f648ad6afe313a287961cb6ce3a8
Pasquale Lovicu <pasquale.lovicu@alice.it>
2024-11-11 18:31:25 +0100

    Inserita Route Registrazione Utenti

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+   public function showRegistrationForm()
+   {
+       return view('register');
+   }
+   public function register(Request $request)
+   {
+
+       $validatedData = $request->validate([
+           'name' => 'required|string|max:255',
+           'email' => 'required|string|email|max:255|unique:users,email',
+           'password' => 'required|string|min:6',
+
+       ]);
+
+       $user = User::create ([
+           'name'=>$validatedData['name'],
+           'email'=>$validatedData['email'],
+           'password'=> Hash::make($validatedData['password']),
+       ]);
+       Auth::login($user);
+       return redirect()->route('showRegisterForm')->with('success','Registrazione avvenuta con successo');
+   }
+
+
+}
